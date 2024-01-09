02a0090b143590bd29bf547bddc973cd53018392
Alejandra Oropeza <aleoru0022@gmail.com>
2024-01-09 02:17:21 +0100

    addNumber method and exception added

--- CPP Module 08/ex01/Span.cpp
+++ CPP Module 08/ex01/Span.cpp
@@ -6,7 +6,7 @@
 /*   By: aoropeza <aoropeza@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 18:46:55 by aoropeza          #+#    #+#             */
-/*   Updated: 2024/01/04 14:43:29 by aoropeza         ###   ########.fr       */
+/*   Updated: 2024/01/08 14:22:42 by aoropeza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,7 +52,6 @@ int	Span::shortestSpan(void){
 	int	aux;
 	int	res;
 	
-
 }
 
 int	Span::longestSpan(void){
